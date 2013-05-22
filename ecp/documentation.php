c5c241a6393068ff5b5d5bc1e10872e03e05e7ec
emueller <edgar.m.mueller@gmail.com>
2013-05-22 20:49:31 +0200

    new site uses static files

--- ecp/documentation.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once '/lib/Twig/Autoloader.php';
-Twig_Autoloader::register();
-$loader = new Twig_Loader_Filesystem('.');
-$twig = new Twig_Environment($loader);
-
-echo $twig->render("documentation.html", array('a' => 'b'));
-?>
