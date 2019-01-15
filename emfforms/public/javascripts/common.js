b2b80f922b7f6dc245c5e5a529d5063a0185d73f
Edgar Mueller <emueller@eclipsesource.com>
2019-01-15 12:12:06 +0100

    Remove deprecated twitter initialization code

--- emfforms/public/javascripts/common.js
+++ emfforms/public/javascripts/common.js
@@ -17,26 +17,3 @@ var getCookies = function(name){
   }
   return cookies;
 }
-
-function createTimeline() {
-  var twitterContainer = document.getElementById("twitter-container");
-  twttr.widgets.createTimeline(
-    "540187881671634945",
-    twitterContainer,
-    {
-      height: 400
-    }
-  );
-  twitterContainer.innerText = '';
-}
-
-function createTwitterWidget() {
-  if (getCookie("eclipse_cookieconsent_status", "allow")) {
-    $.getScript(
-      "https://platform.twitter.com/widgets.js",
-      function () {
-        createTimeline()
-      }
-    );
-  }
-}
